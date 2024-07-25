af9b5ca90b0233844b4d0bf780db51af7e38889b
lo-franky <lo.frankyl@gmail.com>
2024-07-24 19:48:20 -0500

    all routes work and all views have nav bar

--- models/workout.js
+++ models/workout.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const workoutSchema = new mongoose.Schema({
   type: {
     type: String,
-    enum: ['run', 'weightTraining', ],
+    enum: ['run', 'weightTraining', 'walk'],
     required: true
   },
   date: {
